a8326d4eb8a22d7ea6a4154631e63ff6e6fd74a4
Tiago Freire <tiago.freire.2002@gmail.com>
2025-03-25 20:58:17 -0300

    add Prisma integration and user model with database migrations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service'
 import { UsersModule } from './user/user.module'
 import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ConfigModule } from '@nestjs/config'
     }),
     UsersModule,
     AuthModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,40 +1,31 @@
 import { Injectable } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 // This should be a real class/interface representing a user entity
 export type User = any
 
 @Injectable()
 export class UserService {
-  private readonly users: CreateUserDto[] = [
-    {
-      id: 1,
-      email: '1@adore.com',
-      firstName: 'One',
-      lastName: 'Test',
-      password: '',
-    },
-    {
-      id: 2,
-      email: '2@adore.com',
-      firstName: 'Two',
-      lastName: 'Test',
-      password: '',
-    },
-  ]
+  constructor(private prisma: PrismaService) {}
 
   create(user: CreateUserDto) {
-    this.users.push(user)
+    const createdUser = this.prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.firstName,
+      },
+    })
 
-    return this.users.find((u) => u.email === user.email)
+    return createdUser
   }
 
   findByEmail(email: string) {
-    return this.users.find((user) => user.email === email)
+    return this.prisma.user.findUnique({ where: { email } })
   }
 
   updateHashedRefreshToken(userId: number, hashedRefreshToken: string) {
-    const currentUser = this.users.find((user) => user.id === userId)
+    const currentUser = this.prisma.user.findUnique({ where: { id: userId } })
     return currentUser
   }
 }
