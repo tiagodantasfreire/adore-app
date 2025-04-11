8aceb8ae588bc1a29c80cddaabccec2a9065adcb
Tiago Freire <tiago.freire.2002@gmail.com>
2025-04-10 23:01:00 -0300

    feat: wrap ministry components in Suspense for improved loading state with skeleton placeholder

--- web/src/app/ministerio/page.tsx
+++ web/src/app/ministerio/page.tsx
@@ -1,16 +1,21 @@
+import { Suspense } from 'react'
+
 import CreateMinistryButton from '@/components/ministries-list/create-ministry-button'
 import MinistryList from '@/components/ministries-list'
 import SearchInput from '@/components/ministries-list/search-input'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <div className="flex items-center justify-between w-full gap-4">
-        <SearchInput />
-        <CreateMinistryButton />
-      </div>
+      <Suspense fallback={<Skeleton className="w-full h-10" />}>
+        <div className="flex items-center justify-between w-full gap-4">
+          <SearchInput />
+          <CreateMinistryButton />
+        </div>
 
-      <MinistryList />
+        <MinistryList />
+      </Suspense>
     </div>
   )
 }
