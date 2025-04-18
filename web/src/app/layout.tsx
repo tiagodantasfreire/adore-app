b1005589c7eb17cb79154779ce7e3b8a9874c1ad
Tiago Freire <tiago.freire.2002@gmail.com>
2025-04-18 14:20:41 -0300

    refactor: update layout and theme provider to use pt-BR language and integrate Toaster component

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -5,7 +5,6 @@ import { ptBR } from 'date-fns/locale'
 
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { QueryProvider } from '@/components/providers/query-provider'
-import { Toaster } from '@/components/ui/sonner'
 import './globals.css'
 
 setDefaultOptions({ locale: ptBR })
@@ -26,8 +25,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} antialiased`}>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body
+        className={`${inter.variable} antialiased`}
+        suppressHydrationWarning
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -35,7 +37,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <QueryProvider>{children}</QueryProvider>
-          <Toaster />
         </ThemeProvider>
       </body>
     </html>
--- web/src/components/providers/theme-provider.tsx
+++ web/src/components/providers/theme-provider.tsx
@@ -3,9 +3,16 @@
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
+import { Toaster } from '@/components/ui/sonner'
+
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+      <Toaster richColors position="top-center" />
+    </NextThemesProvider>
+  )
 }
