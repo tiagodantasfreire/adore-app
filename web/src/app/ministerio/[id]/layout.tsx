1607ff3c8b766ecf3b09962865610b74acb28059
Tiago Freire <tiago.freire.2002@gmail.com>
2025-04-24 20:05:36 -0300

    fix: adjust padding in MinistryLayout component for improved layout

--- web/src/app/ministerio/[id]/layout.tsx
+++ web/src/app/ministerio/[id]/layout.tsx
@@ -6,7 +6,7 @@ import { NavigationBar } from '@/components/navigation-bar'
 export default function MinistryLayout({ children }: PropsWithChildren) {
   return (
     <MinistryProvider>
-      <main className="p-4 pb-20">{children}</main>
+      <main className="p-4 pb-22">{children}</main>
 
       <NavigationBar />
     </MinistryProvider>
